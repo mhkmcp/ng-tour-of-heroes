7defa76265c06534b92a1fae454702f22988a968
mhkmcp <mhkmcp@yahoo.com>
2021-05-12 14:24:10 +0600

    Added Services & Messages

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../hero';
 import { HEROES } from '../../mock-heroes';
+import { HeroService } from '../../services/hero.service';
+import { MessageService } from '../../services/message.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -10,15 +13,22 @@ import { HEROES } from '../../mock-heroes';
 export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
 
-  heroes: Hero[] = HEROES;
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero:Hero) {
-    this.selectedHero = hero
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.heroes
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
